6ac6094122e776e2eb405d7c3704e822c3a39ad1
tainasays <thaynasant.ana@hotmail.com>
2025-07-20 16:56:56 -0300

    fix ednaldo endpoint

--- src/test/java/com/example/WebApp/WebAppApplicationTests.java
+++ src/test/java/com/example/WebApp/WebAppApplicationTests.java
@@ -46,7 +46,7 @@ class WebAppApplicationTests {
 
   @Test
   public void shouldReturnEdnaldo() throws Exception {
-    this.mockMvc.perform(get("/Ednaldo"))
+    this.mockMvc.perform(get("/ednaldobarbosa"))
             .andDo(print()).andExpect(status().isOk())
             .andExpect(content()
                     .string(containsString("Hello, Ednaldo!?!?")));
